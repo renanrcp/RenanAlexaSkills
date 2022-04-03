823f1eac32683f04f0a1291bfd8e7228af5b4fe5
Renan Pinheiro <renancamerapinheiro@gmail.com>
2022-04-03 10:45:12 -0300

    feat: add sample skill function

--- src/SampleSkill/Function.cs
+++ src/SampleSkill/Function.cs
@@ -1,3 +1,7 @@
+using Alexa.NET;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Amazon.Lambda.Core;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
@@ -7,8 +11,17 @@ namespace SampleSkill;
 
 public static class Function
 {
-    public static string FunctionHandler(string input, ILambdaContext context)
+    public static SkillResponse FunctionHandler(SkillRequest input/*, ILambdaContext context*/)
     {
-        return input.ToUpper();
+        return input.Request is not IntentRequest intentRequest
+            ? ResponseBuilder.Tell(Messages.NOT_INTENT_TYPE_MESSAGE)
+            : intentRequest.Intent.Name.ToUpper() switch
+            {
+                IntentNames.AMAZON_CANCELINTENT => ResponseBuilder.Tell(Messages.BYE_MESSAGE),
+                IntentNames.AMAZON_STOPINTENT => ResponseBuilder.Tell(Messages.BYE_MESSAGE),
+                IntentNames.AMAZON_HELPINTENT => ResponseBuilder.Tell(Messages.HELP_MESSAGE),
+                IntentNames.SAMPLE_SKILL => ResponseBuilder.Tell(Messages.SAMPLESKILL_MESSAGE),
+                _ => ResponseBuilder.Tell(Messages.CANT_UNDERSTAND_MESSAGE)
+            };
     }
 }
--- test/SampleSkill.Tests/FunctionTest.cs
+++ test/SampleSkill.Tests/FunctionTest.cs
@@ -1,4 +1,6 @@
-using Amazon.Lambda.TestUtilities;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
 using Xunit;
 
 namespace SampleSkill.Tests;
@@ -6,13 +8,134 @@ namespace SampleSkill.Tests;
 public class FunctionTest
 {
     [Fact]
-    public void TestToUpperFunction()
+    public void ShouldTellNotIntentTypeWhenRequestIsNotIntentType()
     {
+        // Act
+        var request = new SkillRequest
+        {
+            Request = new LaunchRequest()
+        };
 
-        // Invoke the lambda function and confirm the string was upper cased.
-        var context = new TestLambdaContext();
-        var upperCase = Function.FunctionHandler("hello world", context);
+        // Arrange
+        var result = Function.FunctionHandler(request);
 
-        Assert.Equal("HELLO WORLD", upperCase);
+        // Assert
+        var plainTextOutputSpeechResult = Assert.IsType<PlainTextOutputSpeech>(result.Response.OutputSpeech);
+        Assert.Equal(Messages.NOT_INTENT_TYPE_MESSAGE, plainTextOutputSpeechResult.Text);
+    }
+
+    [Fact]
+    public void ShouldTellCantUnderstandWhenIntentNameIsNotSampleSkillAndNotBuiltinAmazonNames()
+    {
+        // Act
+        var request = new SkillRequest
+        {
+            Request = new IntentRequest
+            {
+                Intent = new()
+                {
+                    Name = "UNDEFINED"
+                }
+            }
+        };
+
+        // Arrange
+        var result = Function.FunctionHandler(request);
+
+        // Assert
+        var plainTextOutputSpeechResult = Assert.IsType<PlainTextOutputSpeech>(result.Response.OutputSpeech);
+        Assert.Equal(Messages.CANT_UNDERSTAND_MESSAGE, plainTextOutputSpeechResult.Text);
+    }
+
+    [Fact]
+    public void ShouldTellByeMessageWhenIntentNameIsBuiltinAmazonStopIntent()
+    {
+        // Act
+        var request = new SkillRequest
+        {
+            Request = new IntentRequest
+            {
+                Intent = new()
+                {
+                    Name = IntentNames.AMAZON_STOPINTENT
+                }
+            }
+        };
+
+        // Arrange
+        var result = Function.FunctionHandler(request);
+
+        // Assert
+        var plainTextOutputSpeechResult = Assert.IsType<PlainTextOutputSpeech>(result.Response.OutputSpeech);
+        Assert.Equal(Messages.BYE_MESSAGE, plainTextOutputSpeechResult.Text);
+    }
+
+    [Fact]
+    public void ShouldTellByeMessageWhenIntentNameIsBuiltinAmazonCancelIntent()
+    {
+        // Act
+        var request = new SkillRequest
+        {
+            Request = new IntentRequest
+            {
+                Intent = new()
+                {
+                    Name = IntentNames.AMAZON_CANCELINTENT
+                }
+            }
+        };
+
+        // Arrange
+        var result = Function.FunctionHandler(request);
+
+        // Assert
+        var plainTextOutputSpeechResult = Assert.IsType<PlainTextOutputSpeech>(result.Response.OutputSpeech);
+        Assert.Equal(Messages.BYE_MESSAGE, plainTextOutputSpeechResult.Text);
+    }
+
+    [Fact]
+    public void ShouldTellHelpMessageWhenIntentNameIsBuiltinAmazonHelpIntent()
+    {
+        // Act
+        var request = new SkillRequest
+        {
+            Request = new IntentRequest
+            {
+                Intent = new()
+                {
+                    Name = IntentNames.AMAZON_HELPINTENT
+                }
+            }
+        };
+
+        // Arrange
+        var result = Function.FunctionHandler(request);
+
+        // Assert
+        var plainTextOutputSpeechResult = Assert.IsType<PlainTextOutputSpeech>(result.Response.OutputSpeech);
+        Assert.Equal(Messages.HELP_MESSAGE, plainTextOutputSpeechResult.Text);
+    }
+
+    [Fact]
+    public void ShouldTellSampleSkillMessageWhenIntentNameIsSampleSkill()
+    {
+        // Act
+        var request = new SkillRequest
+        {
+            Request = new IntentRequest
+            {
+                Intent = new()
+                {
+                    Name = IntentNames.SAMPLE_SKILL
+                }
+            }
+        };
+
+        // Arrange
+        var result = Function.FunctionHandler(request);
+
+        // Assert
+        var plainTextOutputSpeechResult = Assert.IsType<PlainTextOutputSpeech>(result.Response.OutputSpeech);
+        Assert.Equal(Messages.SAMPLESKILL_MESSAGE, plainTextOutputSpeechResult.Text);
     }
 }
