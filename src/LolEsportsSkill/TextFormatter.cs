62fd83ef6d24194602d4d069d11c23afb8d3af90
Renan Pinheiro <renancamerapinheiro@gmail.com>
2022-04-03 18:54:01 -0300

    feat: add more no games text to text formatter lol esposrts skill

--- src/LolEsportsSkill/TextFormatter.cs
+++ src/LolEsportsSkill/TextFormatter.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace LolEsportsSkill;
 
 public static class TextFormatter
@@ -5,5 +7,19 @@ public static class TextFormatter
     public static string FormatGameDay(string team1, string team2, DateTime gameDate)
     {
         return $"Hoje tem jogo entre {team1} e {team2} às {TimeOnly.FromDateTime(gameDate).Hour} horas";
+    }
+
+    public static string TodayHasNoGame(string team1, string team2, DateTime gameDate)
+    {
+        var time = TimeOnly.FromDateTime(gameDate);
+        var date = DateOnly.FromDateTime(gameDate);
+        var monthName = new CultureInfo("pt-BR").DateTimeFormat.GetMonthName(date.Month);
+
+        return $"Hoje não tem jogo, o próximo jogo é entre {team1} e {team2} dia {date.Day} de {monthName} às {time.Hour} horas";
+    }
+
+    public static string NoGamesScheduled()
+    {
+        return "Não há nenhum jogo agendado";
     }
 }
--- test/LolEsportsSkill.UnitTests/TextFormatterTest.cs
+++ test/LolEsportsSkill.UnitTests/TextFormatterTest.cs
@@ -5,7 +5,7 @@ namespace LolEsportsSkill.UnitTests;
 public class TextFormatterTest
 {
     [Fact]
-    public void ShouldConcatenateTwoTeamsForFormatGameDay()
+    public void ShouldConcatenateTwoTeamsAndDateForFormatGameDay()
     {
         // Arrange
         var team1 = "team1";
@@ -17,5 +17,32 @@ public class TextFormatterTest
 
         // Assert
         Assert.Equal("Hoje tem jogo entre team1 e team2 às 15 horas", result);
+    }
+
+    [Fact]
+    public void ShouldConcatenateTwoTeamsAndDateForTodayHasNoGame()
+    {
+        // Arrange
+        var team1 = "team1";
+        var team2 = "team2";
+        var gameDate = new DateTime(2019, 1, 1, 15, 0, 0);
+
+        // Act
+        var result = TextFormatter.TodayHasNoGame(team1, team2, gameDate);
+
+        // Assert
+        Assert.Equal("Hoje não tem jogo, o próximo jogo é entre team1 e team2 dia 1 de janeiro às 15 horas", result);
+    }
+
+    [Fact]
+    public void ShouldReturnNoGamesScheduled()
+    {
+        // Arrange
+
+        // Act
+        var result = TextFormatter.NoGamesScheduled();
+
+        // Assert
+        Assert.Equal("Não há nenhum jogo agendado", result);
     }
 }
